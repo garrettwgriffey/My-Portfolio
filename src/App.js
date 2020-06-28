a75e8d95d675f0e5b50de56ed6129b9dce3411c9
garrett griffey <garrettwgriffey@gmail.com>
2020-06-28 16:42:53 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import Project from './components/Project'
+
+
+function App(){
+    return (
+        <div>
+            <NavBar />
+            <Project />
+            <p>
+            Garrett Griffey
+            </p>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function NavBar() {
+    return (
+        <header >
+           <h1>Garrett Griffey</h1> 
+           <p>Full Stack Web Development</p>
+        </header>
+    );
+}
+
+ 
+export default NavBar;
